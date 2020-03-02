f10236afb99e2b46039125136ac4cf518e6049cb
tr7zw <tr7zw@live.de>
2020-03-02 22:26:36 +0100

    basic test

--- src/test/java/de/tr7zw/trutils/MapTest.java
+++ src/test/java/de/tr7zw/trutils/MapTest.java
@@ -18,8 +18,13 @@ public class MapTest
         return new TestSuite( MapTest.class );
     }
 
-    public void testApp()
+    public void testListMappings()
     {
-        assertTrue( true );
+    	TRMap<Integer, String> map = TRUtils.createList(10, 20, 30, 40).map(i -> i*i).computeKeyMap(i -> Integer.toHexString(i));
+    	assertEquals(map.size(), 4);
+    	assertEquals(map.get(400), "190");
+    	assertEquals(map.get(1600), "640");
+    	assertEquals(map.get(100), "64");
+    	assertEquals(map.get(900), "384");
     }
 }
